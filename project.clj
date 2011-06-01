75dcb0c75ef6ccbaa5514b63104eb4930005e0cd
myfreeweb <me@myfreeweb.ru>
2011-06-01 20:25:29 +0400

    Working basic routing system

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject ringfinger "1.0.0-SNAPSHOT"
-  :description "Resource-oriented RESTful web development on Ring"
+(defproject ringfinger "0.1.0-SNAPSHOT"
+  :description "Resource-oriented RESTful web development on Clojure + Ring"
   :url "https://github.com/myfreeweb/ringfinger"
   :license {:name "Apache License 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"
             :distribution :repo}
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [ring "0.3.8"]])
+                 [ring "0.3.8"]
+                 [ring-mock "0.1.1"]
+                 [clout "0.4.1"]])
--- src/ringfinger/core.clj
+++ src/ringfinger/core.clj
@@ -1,1 +1,18 @@
-(ns ringfinger.core)
+(ns ringfinger.core
+  (:use clout.core))
+
+(def routes (ref (list
+  {:route (route-compile "/*")
+   :handler (fn [req matches]
+    {:status 404
+     :headers {"Content-Type" "text/plain"}
+     :body "404 Not Found"})})))
+
+(defn defroute [route]
+  (dosync (ref-set routes (conj @routes route))))
+
+(defn app [req]
+  (let [route
+     (first
+        (filter (fn [route] (route-matches (:route route) req)) @routes))]
+     ((:handler route) req (route-matches (:route route) req))))
