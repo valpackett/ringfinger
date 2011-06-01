de9c57ae9a0c9307b47c587c6cc98a9b73a85677
myfreeweb <me@myfreeweb.ru>
2011-06-01 22:06:12 +0400

    Added some tests

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # ringfinger #
-Resource-oriented RESTful web development on top of [Clojure](http://clojure.org) + [Ring](https://github.com/mmcgrana/ring).
-
+Resource-oriented/RESTful web development on top of [Clojure](http://clojure.org) + [Ring](https://github.com/mmcgrana/ring).
+Named after the great Nine Inch Nails song.
--- test/ringfinger/test/core.clj
+++ test/ringfinger/test/core.clj
@@ -1,6 +1,28 @@
 (ns ringfinger.test.core
-  (:use [ringfinger.core])
-  (:use [clojure.test]))
+  (:use ringfinger.core, clojure.test, ring.mock.request))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+;; Routing
+
+(defroute "/test/:a"
+          {:get (fn [req matches]
+                  {:status 200
+                   :headers {"Content-Type" "text/plain"}
+                   :body (str "Yo: " (:a matches))})})
+
+(deftest right-method
+  (is (= (app (request :get "/test/hello"))
+         {:status 200
+          :headers {"Content-Type" "text/plain"}
+          :body "Yo: hello"})))
+
+(deftest wrong-method
+  (is (= (app (request :post "/test/hello"))
+         {:status 405
+          :headers {"Content-Type" "text/plain"}
+          :body "405 Method Not Allowed"})))
+
+(deftest wrong-url
+  (is (= (app (request :get "/test"))
+         {:status 404
+          :headers {"Content-Type" "text/plain"}
+          :body "404 Not Found"})))
