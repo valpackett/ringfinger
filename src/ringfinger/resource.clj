413e17c2892cded765c738510d46f94bcd9b2cb3
myfreeweb <me@myfreeweb.ru>
2011-06-02 17:29:20 +0400

    DRY'd qsformat

--- src/ringfinger/resource.clj
+++ src/ringfinger/resource.clj
@@ -6,6 +6,12 @@
 (defn- keywordize [a]
   (zipmap (map (fn [b] (keyword b)) (keys a)) (vals a)))
 
+(defn- qsformat [req]
+  (let [format (first (select-keys (:query-params req) ["format"]))]
+    (if format
+      (str "?" (first format) "=" (get format 1))
+      "")))
+
 (defn defresource [coll options fields]
   (defroute (str "/" (as-str coll))
     {;:get (fn [req matches]
@@ -14,8 +20,7 @@
      :post (fn [req matches]
              (let [form (keywordize (:form-params req))]
                (create (:store options) coll form)
-               (redirect (str "/" (as-str coll) "/" (get form (:pk options)) "?" (:query-string req)))
-                ; FIXME: get only format instead of the query string
+               (redirect (str "/" (as-str coll) "/" (get form (:pk options)) (qsformat req)))
                ))})
   (defroute (str "/" (as-str coll) "/:pk")
     {:get (fn [req matches]
@@ -25,7 +30,7 @@
      :put (fn [req matches]
             (let [form (keywordize (:form-params req))]
               (update (:store options) coll (get_one (:store options) coll (:pk options) (:pk matches)) form)
-              (redirect (str "/" (as-str coll) "/" (get form (:pk options)) "?" (:query-string req)))))
+              (redirect (str "/" (as-str coll) "/" (get form (:pk options)) (qsformat req)))))
      :delete (fn [req matches]
                (delete (:store options) coll (get_one (:store options) coll (:pk options) (:pk matches)))
                {:status  200
