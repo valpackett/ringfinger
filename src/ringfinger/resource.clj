0a2e975edfb70e5e119c76bdf201f639e9077684
myfreeweb <me@myfreeweb.ru>
2011-06-02 17:48:28 +0400

    DRY FTW

--- src/ringfinger/resource.clj
+++ src/ringfinger/resource.clj
@@ -13,26 +13,28 @@
       "")))
 
 (defn defresource [coll options fields]
+  (let [store (:store options)
+        i_get_one (fn [matches] (get_one store coll (:pk options) (:pk matches)))
+        i_redirect (fn [req form] (redirect (str "/" (as-str coll) "/" (get form (:pk options)) (qsformat req))))]
   (defroute (str "/" (as-str coll))
     {;:get (fn [req matches]
             ; TODO: index
     ;        )
      :post (fn [req matches]
              (let [form (keywordize (:form-params req))]
-               (create (:store options) coll form)
-               (redirect (str "/" (as-str coll) "/" (get form (:pk options)) (qsformat req)))
-               ))})
+               (create store coll form)
+               (i_redirect req form)))})
   (defroute (str "/" (as-str coll) "/:pk")
     {:get (fn [req matches]
             {:status 200
              :headers {"Content-Type" "application/json"}
-             :body (json-str (get_one (:store options) coll (:pk options) (:pk matches)))}) ; TODO: DRY this
+             :body (json-str (i_get_one matches))})
      :put (fn [req matches]
             (let [form (keywordize (:form-params req))]
-              (update (:store options) coll (get_one (:store options) coll (:pk options) (:pk matches)) form)
-              (redirect (str "/" (as-str coll) "/" (get form (:pk options)) (qsformat req)))))
+              (update store coll (i_get_one matches) form)
+              (i_redirect req form)))
      :delete (fn [req matches]
-               (delete (:store options) coll (get_one (:store options) coll (:pk options) (:pk matches)))
+               (delete store coll (i_get_one matches))
                {:status  200
                 :headers {"Content-Type" "application/json"}
-                :body "{}"})}))
+                :body "{}"})})))
