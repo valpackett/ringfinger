25cfc340787ca2cf36daadfe905d7ee9a62646c6
myfreeweb <me@myfreeweb.ru>
2011-06-07 16:24:04 +0400

    Ring DB session store

---
+++ src/ringfinger/session.clj
@@ -0,0 +1,14 @@
+(ns ringfinger.session
+  (:use ring.middleware.session.store,
+        ringfinger.db))
+
+(deftype DBStore [db coll] SessionStore
+  (read-session   [self key] (get_one db coll {:_id key}))
+  (write-session  [self key data] (let [ex (get_one db coll {:_id key})]
+                                    (if ex (update db coll ex data)
+                                           (create db coll data))))
+  (delete-session [self key] (delete db coll key)))
+
+(defn db-store
+  ([db] (db-store db :ringfinger_sessions))
+  ([db coll] (DBStore. db coll)))
