c68525e61fdd468e08bbec8bd38e62a43524eb8d
myfreeweb <me@myfreeweb.ru>
2011-06-07 16:38:51 +0400

    Session ID generation

--- src/ringfinger/session.clj
+++ src/ringfinger/session.clj
@@ -1,12 +1,13 @@
 (ns ringfinger.session
   (:use ring.middleware.session.store,
-        ringfinger.db))
+        ringfinger.db)
+  (:import [java.util UUID Date]))
 
 (deftype DBStore [db coll] SessionStore
   (read-session   [self key] (get_one db coll {:_id key}))
   (write-session  [self key data] (let [ex (get_one db coll {:_id key})]
                                     (if ex (update db coll ex data)
-                                           (create db coll data))))
+                                           (create db coll (merge data {:_id (UUID/randomUUID)})))))
   (delete-session [self key] (delete db coll key)))
 
 (defn db-store
