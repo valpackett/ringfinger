92a96039d3a3f95e2793166c5e3d917b7e96aa1b
myfreeweb <me@myfreeweb.ru>
2011-06-07 22:35:15 +0400

    Updated tests

--- test/ringfinger/test/resource.clj
+++ test/ringfinger/test/resource.clj
@@ -19,7 +19,7 @@
 (deftest wrong-create
   (is (= (testapp (body (request :post "/todos?format=json") {:state false}))
          {:status  400
-          :headers {"Content-Type" "application/json"}
+          :headers {"Content-Type" "application/json; charset=utf-8"}
           :body    "{\"body\":[\"should be present\"]}"})))
 
 (deftest right-update
@@ -32,24 +32,24 @@
 (deftest right-show
   (is (= (testapp (request :get "/todos/test?format=json"))
          {:status  200
-          :headers {"Content-Type" "application/json"}
+          :headers {"Content-Type" "application/json; charset=utf-8"}
           :body    "{\"state\":true,\"body\":\"test\"}"}))
   (is (= (testapp (header (request :get "/todos/test") "Accept" "text/xml"))
           {:status  200
-          :headers {"Content-Type" "application/xml"}
+          :headers {"Content-Type" "application/xml; charset=utf-8"}
           :body    "<?xml version=\"1.0\" encoding=\"UTF-8\" ?><response><state>true</state><body>test</body></response>"})))
 
 (deftest right-index
   (is (= (testapp (request :get "/todos?format=json"))
          {:status  200
-          :headers {"Content-Type" "application/json"}
+          :headers {"Content-Type" "application/json; charset=utf-8"}
           :body "[{\"state\":true,\"body\":\"test\"}]"})))
 
 (deftest right-delete
   (is (= (testapp (request :delete "/todos/test?format=json"))
-          {:status  200
-           :headers {"Content-Type" "application/json"}
-           :body    "{}"}))
+          {:status  302
+           :headers {"Location" "/todos"}
+           :body    ""}))
   (is (= (get_one inmem :todos {:body "test"}) nil)))
 
 (defn test-ns-hook []
