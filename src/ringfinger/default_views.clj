f7b9334bde8f1eee840ffd65a90d64797d78ab30
myfreeweb <me@myfreeweb.ru>
2011-08-11 13:24:49 +0400

    Moved auth demo views: auth-routes -> default-views

--- src/ringfinger/default_views.clj
+++ src/ringfinger/default_views.clj
@@ -54,3 +54,38 @@
     [:head [:title (str (:collname stuff) " / not found")]
            [:style default-style]]
     [:body [:h1 "Not found :-("]]]))
+
+(def auth-demo-views
+  {:login  (fn [stuff] (let [errs (:errors stuff)] (html5 [:html
+    [:head [:title "Log in"]
+           [:style default-style]]
+    [:body
+     [:h1 "Log in"]
+     (p-flash stuff)
+     [:form {:method "post" :action (:action stuff)}
+      (form-fields (:fields stuff) (:data stuff) (:errors stuff) [:div] [:div {:class "error"}] :placeholder)
+      (p-csrftoken stuff)
+      [:button {:type "submit"} "Log in!"]]]])))
+   :signup (fn [stuff] (html5 [:html
+    [:head [:title "Sign up"]
+           [:style default-style]]
+    [:body
+     [:h1 "Sign up"]
+     (p-flash stuff)
+     [:form {:method "post" :action (:action stuff)}
+      (form-fields (:fields stuff) (:data stuff) (:errors stuff) [:div] [:div {:class "error"}] :placeholder)
+      (p-csrftoken stuff)
+      [:button {:type "submit"} "Sign up!"]
+      ]]]))
+   :confirm (fn [stuff] (html5 [:html
+    [:head [:title "Confirm"]
+           [:style default-style]]
+    [:body
+     [:h1 "Confirm"]
+     (p-flash stuff)
+     "Check your email."
+     ]]))
+   })
+
+(defn demo-mail-template [data]
+  (str "Welcome! To activate your account, click this link: " (:url data)))
