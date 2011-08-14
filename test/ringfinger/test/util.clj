1f558543173cf0339031b61e98df04c90ab0217d
myfreeweb <me@myfreeweb.ru>
2011-08-14 15:05:07 +0400

    haz test

--- test/ringfinger/test/util.clj
+++ test/ringfinger/test/util.clj
@@ -5,3 +5,7 @@
   (let [m [{:a 1 :b 1} {:a 1 :b 2} {:a 2 :b 1} {:a 2 :b 2}]]
     (is (= (sort-maps m {:a 1 :b -1}) '({:a 1 :b 2} {:a 1 :b 1} {:a 2 :b 2} {:a 2 :b 1})))
     (is (= (sort-maps m {:a -1 :b 1}) '({:a 2 :b 1} {:a 2 :b 2} {:a 1 :b 1} {:a 1 :b 2})))))
+
+(deftest t-haz
+  (is (= (haz? [:a :b] :a) true))
+  (is (= (haz? [:a :b] :c) false)))
