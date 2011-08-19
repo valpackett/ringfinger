be8fdeac3153b741b77185ed21a7e3da1d5dd1d7
myfreeweb <me@myfreeweb.ru>
2011-08-19 18:17:13 +0400

    nice-count facts

--- test/ringfinger/test/util.clj
+++ test/ringfinger/test/util.clj
@@ -1,6 +1,12 @@
 (ns ringfinger.test.util
   (:use ringfinger.util, midje.sweet))
 
+(facts "about nice-count"
+  (nice-count 0 "post") => "no posts"
+  (nice-count 1 "post") => "one post"
+  (nice-count 2 "post") => "two posts"
+  (nice-count 99 "problem") => "99 problems")
+
 (facts "about sort-maps"
   (let [m [{:a 1 :b 1} {:a 1 :b 2} {:a 2 :b 1} {:a 2 :b 2}]]
     (sort-maps m {:a 1 :b -1}) => '({:a 1 :b 2} {:a 1 :b 1} {:a 2 :b 2} {:a 2 :b 1})
