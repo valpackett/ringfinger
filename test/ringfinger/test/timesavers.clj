776935e9700aeb9174c225ffffb4352e27f3d353
myfreeweb <me@myfreeweb.ru>
2011-08-21 20:56:09 +0400

    nicer formatting in timesavers test

--- test/ringfinger/test/timesavers.clj
+++ test/ringfinger/test/timesavers.clj
@@ -6,7 +6,7 @@
 (fact ((safe-html :s) {:s "<b><script>alert('xss lol');</script></b>"}) => {:s "<b></b>"})
 
 (facts "about escape-input"
-  ((escape-input :i :html) {:i "<tag></tag>"})    => {:i "&lt;tag&gt;&lt;&#x2F;tag&gt;"}
+  ((escape-input :i :html)    {:i "<tag></tag>"}) => {:i "&lt;tag&gt;&lt;&#x2F;tag&gt;"}
   ((escape-input :i :urlpart) {:i "hello world"}) => {:i "hello+world"}
-  ((escape-input :i :js) {:i "Hi :)"})            => {:i "Hi\\x20\\x3a\\x29"}
-  ((escape-input :i :css) {:i "yo "})             => {:i "yo\\20"})
+  ((escape-input :i :js)      {:i "Hi :)"})       => {:i "Hi\\x20\\x3a\\x29"}
+  ((escape-input :i :css)     {:i "yo "})         => {:i "yo\\20"})
