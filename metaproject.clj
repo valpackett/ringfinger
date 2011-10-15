7c280d4b02ede1fe9a3be44a12b76856cadc4cbb
myfreeweb <me@myfreeweb.ru>
2011-10-15 18:54:35 +0400

    ugh

--- metaproject.clj
+++ metaproject.clj
@@ -1,9 +1,9 @@
 (use 'clojure.java.shell)
 
 ; Ringfinger's version is defined by git tags
+(if (= (System/getenv "HAS_JOSH_K_SEAL_OF_APPROVAL") "true")
+  (sh "git" "checkout" "master")) ; Travis CI, Y U CHECKOUT REVISION
 (def version
-  (if (= (System/getenv "HAS_JOSH_K_SEAL_OF_APPROVAL") "true")
-    (sh "git" "checkout" "master")) ; Travis CI, Y U CHECKOUT REVISION
   (let [out (:out (sh "git" "describe" "--abbrev=0" "--tags"))]
     (.substring out 0 (- (count out) 1)))) ; remove the \n
 (def clj-version "1.2.1")
