0f00d83a5f289fc1bec60e9060ed2a62421e20df
myfreeweb <me@myfreeweb.ru>
2011-10-16 16:28:30 +0400

    sendmail support

---
+++ mailfinger/src/mailfinger/sendmail.clj
@@ -0,0 +1,17 @@
+(ns mailfinger.sendmail)
+
+(def first-sendmail
+  (first (filter #(.isFile (java.io.File. %))
+    (map #(str "/usr/" % "/sendmail")
+         ["sbin" "bin" "local/sbin" "local/bin"]))))
+
+(defn sendmail
+  "Creates a new Sendmail mailer function"
+  ([] (sendmail first-sendmail))
+  ([sendmail-path])
+    (fn [from to subject body]
+      (let [process (.start (ProcessBuilder. [sendmail-path (str "-f " from) to]))
+            stream (java.io.PrintStream. (.getOutputStream process))]
+              (.print stream (format "Subject: %s\nContent-Type: text/plain;encoding=utf-8\n\n%s" subject body))
+              (.close stream)
+              (.waitFor process))))
